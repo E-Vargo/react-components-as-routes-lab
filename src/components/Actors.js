5e787f49b1d0ebf0f02c87a6b27f93fea258d9e7
E-Vargo <vargoelliott@gmail.com>
2022-09-29 18:41:20 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,15 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div>{actor.name}
+            {actor.movies.map(movie => (
+              <p>{movie}</p>
+            ))}
+        </div>
+       
+      ))}
     </div>
   );
 };
